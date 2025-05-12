e42356cd3d1dc92e31603e9bbe6a7c1cbfaafef4
udoka <udokaineh04@gmail.com>
2025-05-12 07:10:17 +0100

    Updated text

--- src/components/about/Team.jsx
+++ src/components/about/Team.jsx
@@ -16,8 +16,6 @@ const Team = () => {
                     <div className="flex flex-col justify-center items-center gap-[8px]">
                         <p className="text-[20px] md:text-[16px] text-center font-semibold">Johnson Chukwuemeka Okafor</p>
                         <p className="text-[16px] text-blue font-medium">President, CEO & Founder</p>
-                        <p className="text-[20px] md:text-[16px] text-center font-semibold">Johnson Chukwuemeka Okafor</p>
-                        <p className="text-[16px] text-blue font-medium">President, CEO & Founder</p>
                     </div>
                 </div>
 
@@ -26,16 +24,12 @@ const Team = () => {
                     <div className="flex flex-col justify-center items-center gap-[8px]">
                         <p className="text-[20px] font-semibold md:text-[16px] text-center">Joy Ohumagu-Mgbenyere Johnson</p>
                         <p className="text-[16px] text-blue">Country Director</p>
-                        <p className="text-[20px] font-semibold md:text-[16px] text-center">Joy Ohumagu-Mgbenyere Johnson</p>
-                        <p className="text-[16px] text-blue">Country Director</p>
                     </div>
                 </div>
 
                 <div className="flex flex-col justify-start items-center gap-[16px]">
                     <img className="w-[350px] h-[250px] lg:h-[220px] rounded-[8px] md:w-[283px] md:h-[200px] object-cover border" src={team3} alt="" />
                     <div className="flex flex-col justify-center items-center gap-[8px]">
-                        <p className="text-[20px] font-semibold md:text-[16px] text-center">Godwin Ekah Okoh</p>
-                        <p className="text-[16px] text-blue">Board Member</p>
                         <p className="text-[20px] font-semibold md:text-[16px] text-center">Godwin Ekah Okoh</p>
                         <p className="text-[16px] text-blue">Board Member</p>
                     </div>
